1b31a4f9cf314a75d94f285e99752744f662b3a2
Ameer Ul Aman <w.ameerulaman@gmail.com>
2025-01-24 23:01:52 +0500

    init file reading

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/delaney_solubility_with_descriptors.csv")
+
+print(df)
