447aef43b08e76e0611814dd2d81fc516e4e9f59
AmeerUlAman <w.ameerulaman@gmail.com>
2025-01-28 02:34:14 +0500

    csv_save + unneccomments del

--- main.py
+++ main.py
@@ -31,5 +31,9 @@ lr_test_r2 = r2_score(y_test,y_test_pred)
 
 lr_results=pd.DataFrame(["Linear Regression Results",lr_train_mse,lr_train_mse,lr_test_mse,lr_test_r2]).transpose()
 lr_results.columns = ["Model","Training MSE","Training r2","Testing MSE","Testing r2"]
+lr_results.to_csv("linear_regression_results.csv", index=False)
+
+ 
+df.to_csv("orig_data", index=False)
 
 print(lr_results)
